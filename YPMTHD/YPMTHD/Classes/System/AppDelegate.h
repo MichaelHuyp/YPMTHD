c345321cedd88939f7636e1042b6c4ad244e2d7b
MichaelHuyp <86812684@qq.com>
2015-11-29 20:36:56 +0800

    init
    init

---
+++ YPMTHD/YPMTHD/Classes/System/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YPMTHD
+//
+//  Created by 胡云鹏 on 15/11/29.
+//  Copyright © 2015年 tyiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
